e38feaa6778a0c0897525578227cbedabe614820
Michal Zaig <Michalzaig100@gmail.com>
2022-05-01 16:06:02 +0300

    Initial commit

---
+++ Project_Michal_Hodaya/Michal_Hodaya/Project_Qeue/DataAcess/Data/RegistrationToTheSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcess.Data
+{
+    public class RegistrationToTheSystem
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public string BusinessName { get; set; }
+        [Required]
+        public string TypeOfBusiness { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public int MobilePhoneNumber { get; set; }
+
+    }
+}
---
+++ Project_Michal_Hodaya/Michal_Hodaya/Project_Qeue/DataAcess/Migrations/20220315111659_AddBusinessDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAcess.Migrations
+{
+    public partial class AddBusinessDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "List",
+                columns: table => new
+                {
+                    BusinessName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    TypeOfBusiness = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MobilePhoneNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_List", x => x.BusinessName);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "List");
+        }
+    }
+}
